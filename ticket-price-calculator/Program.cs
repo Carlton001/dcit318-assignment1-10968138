e37e29cddf116114f85c3dd053c875aac522c91d
Carlton001 <carltonakussah10@gmail.com>
2024-06-02 11:35:36 +0000

    second commit

---
+++ ticket-price-calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace TicketPriceCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Enter age: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            if(x >= 65 || x <= 12){
+                Console.WriteLine("Your ticket price is GHC7");
+            }
+            else{
+                Console.WriteLine("your ticket price is GHC10");
+            }
+        }
+    }
+
+}
